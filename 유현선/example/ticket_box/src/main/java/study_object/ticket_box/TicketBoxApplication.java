78de231cb07c2e02d4a1555da52ad3c9547971a7
itosamto <itosatmo.hshsy@gmail.com>
2023-08-27 19:49:28 +0900

    init example

---
+++ 유현선/example/ticket_box/src/main/java/study_object/ticket_box/TicketBoxApplication.java
@@ -0,0 +1,13 @@
+package study_object.ticket_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBoxApplication.class, args);
+	}
+
+}
