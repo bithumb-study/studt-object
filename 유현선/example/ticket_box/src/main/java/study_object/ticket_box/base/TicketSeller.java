3a13e5eb5ec6d74f011ad658cc56c209c87ff5c5
itosamto <itosatmo.hshsy@gmail.com>
2023-08-27 22:00:35 +0900

    fix: 캡슐화1 Theater -> TicketSeller

--- 유현선/example/ticket_box/src/main/java/study_object/ticket_box/base/TicketSeller.java
+++ 유현선/example/ticket_box/src/main/java/study_object/ticket_box/base/TicketSeller.java
@@ -1,13 +1,21 @@
 package study_object.ticket_box.base;
 
 public class TicketSeller {
-    public TicketOffice ticketOffice;
+    private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
