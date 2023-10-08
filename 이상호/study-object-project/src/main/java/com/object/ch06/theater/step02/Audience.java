40924d8c6a2209ad09938f283e70379908acf5aa
michol.lee <lovethefeel@gmail.com>
2023-10-08 21:36:37 +0900

    Revert "Revert "docs: chapter 06 이상호 정리글 업로드""
    This reverts commit 121ad8750841f888842f1897a23289f85ec99584.

---
+++ 이상호/study-object-project/src/main/java/com/object/ch06/theater/step02/Audience.java
@@ -0,0 +1,13 @@
+package com.object.ch06.theater.step02;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long setTicket(Ticket ticket) {
+        return bag.setTicket(ticket);
+    }
+}
