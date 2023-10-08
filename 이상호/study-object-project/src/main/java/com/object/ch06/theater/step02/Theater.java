f2467f8bebda74ef15c48e70ae272c5670ad985c
codeleesh(이상호) <38518639+codeleesh@users.noreply.github.com>
2023-10-08 21:11:23 +0900

    Revert "Revert "docs: chapter 06 이상호 정리글 업로드""

---
+++ 이상호/study-object-project/src/main/java/com/object/ch06/theater/step02/Theater.java
@@ -0,0 +1,13 @@
+package com.object.ch06.theater.step02;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        ticketSeller.setTicket(audience);
+    }
+}
